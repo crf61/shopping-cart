39d4efad7674328094b3f3e2ae74f29c979c0c05
crf61 <crf61@georgetown.edu>
2020-04-21 00:34:35 -0400

    Removing the string of code

--- shopping_cart.py
+++ shopping_cart.py
@@ -10,112 +10,112 @@
 #A friendly message thanking the customer and/or encouraging the customer to shop again
 
 
-products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
-
 def to_usd(my_price):
     return f"${my_price:,.2f}"
 
 if __name__ == "__main__":
+    products = [
+        {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+        {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+        {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
+        {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
+        {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
+        {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
+        {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
+        {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
+        {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
+        {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
+        {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
+        {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
+        {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
+        {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
+        {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
+        {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+        {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
+        {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
+        {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
+        {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
+    ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+
     # TODO: write some Python code here to produce the desired output
 
 
-#
-# Info capture / Input
-# 
-total_price = 0
-selected_ids = []
+    #
+    # Info capture / Input
+    # 
+    total_price = 0
+    selected_ids = []
 
-while True:
-        selected_id = input("Please enter a product identifier: ") 
-        if selected_id == "DONE":
-                break
-        else:
-                #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-                #matching_product = matching_products[0]
-                #total_price = total_price + matching_product["price"]
-                #print("SELECTED PRODUCTED: " + matching_product["name"] + " " + str(matching_product["price"])) 
-                selected_ids.append(selected_id) 
-
-
-
-#
-# Info capture / Output
-#
+    while True:
+            selected_id = input("Please enter a product identifier: ") 
+            if selected_id == "DONE":
+                    break
+            else:
+                    #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+                    #matching_product = matching_products[0]
+                    #total_price = total_price + matching_product["price"]
+                    #print("SELECTED PRODUCTED: " + matching_product["name"] + " " + str(matching_product["price"])) 
+                    selected_ids.append(selected_id) 
 
 
-# print(selected_ids)
+
+    #
+    # Info capture / Output
+    #
 
 
-import datetime
-x = datetime.datetime.now ()
+    # print(selected_ids)
 
-for selected_id in selected_ids:
-            matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-            matching_product = matching_products[0]
-            total_price = total_price + matching_product["price"]
-            print("SELECTED PRODUCTED: " + matching_product["name"] + " " + str(matching_product["price"]))  
 
-taxrate = 0.065
-tax = total_price * taxrate
-sum_total = tax + total_price
+    import datetime
+    x = datetime.datetime.now ()
 
-def lookup_product_by_id(selected_id):
-    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-    return matching_products[0]
+    for selected_id in selected_ids:
+                matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+                matching_product = matching_products[0]
+                total_price = total_price + matching_product["price"]
+                print("SELECTED PRODUCTED: " + matching_product["name"] + " " + str(matching_product["price"]))  
 
-price = 0
+    taxrate = 0.065
+    tax = total_price * taxrate
+    sum_total = tax + total_price
 
-print("----------------------------------------")
-print("CHRISTY'S BODEGA IN AREA FOUR")
-print("----------------------------------------")
-print("Website: TrapBodega.com")
-print("Phone: 123-456-7890")
-print ("Date:", x)
-print("----------------------------------------")
-print("What's in Your Basket: ")
-for selected_id in selected_ids:
-    product = lookup_product_by_id(selected_id)
-    price += product["price"]
-    price_usd = ' (${0:.2f})'.format(product["price"])
-    print(" + " + product["name"] + price_usd)
-#Christy was unable to figure this out
-print("----------------------------------------")
-print (f"Amount of purchase: {to_usd(total_price)}")
-#calculate tax
+    def lookup_product_by_id(selected_id):
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        return matching_products[0]
 
-print (f"plus MA Sales Tax (6.25%): {to_usd(tax)}")
+    price = 0
 
-print (f"The total price is:, {to_usd(sum_total)}")
-print("----------------------------------------")
+    print("----------------------------------------")
+    print("CHRISTY'S BODEGA IN AREA FOUR")
+    print("----------------------------------------")
+    print("Website: TrapBodega.com")
+    print("Phone: 123-456-7890")
+    print ("Date:", x)
+    print("----------------------------------------")
+    print("What's in Your Basket: ")
+    for selected_id in selected_ids:
+        product = lookup_product_by_id(selected_id)
+        price += product["price"]
+        price_usd = ' (${0:.2f})'.format(product["price"])
+        print(" + " + product["name"] + price_usd)
+    #Christy was unable to figure this out
+    print("----------------------------------------")
+    print (f"Amount of purchase: {to_usd(total_price)}")
+    #calculate tax
 
-print(f"Thank you for shopping at Christy's Bodega. Have a wonderful day. I love you")
+    print (f"plus MA Sales Tax (6.25%): {to_usd(tax)}")
 
-#A grocery store name of your choice
-#A grocery store phone number and/or website URL and/or address of choice
-#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
-#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
-#The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
-#The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-#The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-#A friendly message thanking the customer and/or encouraging the customer to shop again
+    print (f"The total price is:, {to_usd(sum_total)}")
+    print("----------------------------------------")
+
+    print(f"Thank you for shopping at Christy's Bodega. Have a wonderful day. I love you")
+
+    #A grocery store name of your choice
+    #A grocery store phone number and/or website URL and/or address of choice
+    #The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
+    #The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
+    #The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
+    #The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
+    #The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+    #A friendly message thanking the customer and/or encouraging the customer to shop again
--- test/my_test.py
+++ test/my_test.py
@@ -1,7 +1,7 @@
 
 import pytest # for pytest.raises (see: https://docs.pytest.org/en/latest/assert.html)
 
-from shopping_cart import to_usd, find_product
+from shopping_cart import to_usd 
 
 def test_to_usd():
     # it should apply USD formatting
@@ -15,18 +15,3 @@ def test_to_usd():
 
     # it should display thousands separators
     assert to_usd(1234567890.5555555) == "$1,234,567,890.56"
-
-def test_find_product():
-    products = [
-        {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-        {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-        {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    ]
-
-    # if there is a match, it should find and return a product
-    matching_product = find_product("2", products)
-    assert matching_product["name"] == "All-Seasons Salt"
-
-    # if there is no match, it should raise an IndexError
-    with pytest.raises(IndexError):
-        find_product("2222", products)
